ff2583b7828579a1865d957717cec0400a66146f
MyCode07 <erem19992015@mail.ru>
2024-05-09 15:18:50 +0300

    add new pokytie and variation tabs t material tabs

--- src/js/parts/tabs.js
+++ src/js/parts/tabs.js
@@ -1,6 +1,5 @@
 const tabAreas = document.querySelectorAll('[data-tabs-area]');
 
-
 if (tabAreas.length) {
     tabAreas.forEach(area => {
         const tabs = area.querySelectorAll('[data-tab]');
@@ -25,6 +24,44 @@ if (tabAreas.length) {
 
                 content.forEach(item => {
                     const currentId = item.dataset.tabContent;
+
+                    if (currentId == id) {
+                        item.classList.add('_active');
+                    }
+                    else {
+                        item.classList.remove('_active');
+                    }
+                });
+            })
+        })
+    })
+}
+
+const innerTabAreas = document.querySelectorAll('[data-inner-tabs-area]');
+if (innerTabAreas.length) {
+    innerTabAreas.forEach(area => {
+        const tabs = area.querySelectorAll('[data-inner-tab]');
+        const content = area.querySelectorAll('[data-inner-tab-content]');
+
+        tabs.forEach(tab => {
+            const id = tab.dataset.innerTab;
+
+            tab.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                tabs.forEach(item => {
+                    const currentId = item.dataset.innerTab;
+
+                    if (currentId == id) {
+                        item.classList.add('_active');
+                    }
+                    else {
+                        item.classList.remove('_active');
+                    }
+                });
+
+                content.forEach(item => {
+                    const currentId = item.dataset.innerTabContent;
 
                     if (currentId == id) {
                         item.classList.add('_active');
